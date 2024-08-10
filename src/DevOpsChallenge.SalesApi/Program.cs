d42c85674a5b8cd02ab4c90fa9afcce82951fe0c
Pasan Wijerathne <pasanwijerathne@Pasans-Air.lan>
2024-08-10 11:34:37 +0530

    initial commit

---
+++ src/DevOpsChallenge.SalesApi/Program.cs
@@ -0,0 +1,25 @@
+using DevOpsChallenge.SalesApi;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using System;
+
+// Set console title
+Console.Title = "DevOps Challenge Sales API";
+
+// Build and run host
+await Host.CreateDefaultBuilder(args)
+    .ConfigureAppConfiguration(config =>
+    {
+        config.AddEnvironmentVariables("APP_");
+    })
+    .ConfigureWebHostDefaults(webBuilder =>
+    {
+        webBuilder
+            .UseKestrel(options =>
+            {
+                options.AddServerHeader = false;
+            })
+            .UseStartup<Startup>();
+    })
+    .RunConsoleAsync();
